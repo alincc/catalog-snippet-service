b8f365c2a256eaa55742e65be37160265a843e0a
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-11-20 10:53:00 +0100

    BT-3582  - contentsearch feign repo

---
+++ catalog-snippet-service-rest/src/main/java/no/nb/microservices/catalogsnippet/core/iiif/repository/ContentSearchRepository.java
@@ -0,0 +1,16 @@
+package no.nb.microservices.catalogsnippet.core.iiif.repository;
+
+import no.nb.microservices.catalogcontentsearch.rest.model.AnnotationList;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("catalog-content-search-service")
+public interface ContentSearchRepository {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/catalog/contentsearch/{id}/search")
+    AnnotationList search(@PathVariable String id, @RequestParam String q);
+
+}
